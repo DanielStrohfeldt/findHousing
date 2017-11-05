ba6e3d743b9193f3c373d985c4491fa188dd9381
DanielStrohfeldt <daniel.strohfeldt@gmail.com>
2017-11-04 21:45:52 -0500

    Some methods added

--- source/findHousing/find_sf_housing.py
+++ source/findHousing/find_sf_housing.py
@@ -1,22 +1,39 @@
+# Extended Python
 from craigslist import CraigslistHousing
 import yaml
 
-with open('coordinates.yaml', 'r') as coord_lookup:
-    coordinates = yaml.load(coord_lookup) 
+# Created Python
+from utilities import dist_coords
 
-housing = CraigslistHousing(site='sfbay', area='sfc', category='apa',
-        filters={'max_price': 1600, 'min_price':1000,
+with open('neighborhood_bounds.yaml', 'r') as coord_lookup:
+    neighborhood_coords = yaml.load(coord_lookup) 
+
+with open('shuttle_coordinates.yaml', 'r') as coord_lookup:
+    shuttle_coords = yaml.load(coord_lookup)
+
+sf_housing = CraigslistHousing(site='sfbay', area='sfc', category='apa',
+        filters={'max_price': 1700, 'min_price':1000,
                  'has_image': True})
 
-        
-def get_housing_results():
+pen_housing = CraigslistHousing(site='sfbay', area='pen', category='apa',
+        filters={'max_price': 1700, 'min_price':1000,
+                 'has_image': True})
+
+def get_housing_results(location):
     """
     Get the search results
     """
-    housing_found = housing.get_results(sort_by='newest', geotagged=True, limit=20)
-    results = []
-    for result in housing_found:
-        results.append(result)
+    if location == 'sf':
+       housing_found = sf_housing.get_results(sort_by='newest', geotagged=True, limit=20)
+       results = []
+       for result in housing_found:
+          results.append(result)
+    if location == 'pen':
+       housing_found = pen_housing.get_results(sort_by='newest', geotagged=True, limit=20)
+       results = []
+       for result in housing_found:
+          results.append(result)
+
 
     return results
 
@@ -30,7 +47,7 @@ def in_location_specified(search_result, bounding_box):
         return False
 
     lat = coordinates[0]
-    lon = coordinales[1]
+    lon = coordinates[1]
 
     bb_lat_low = bounding_box[0][0]
     bb_lat_up = bounding_box[1][0]
@@ -58,6 +75,62 @@ def in_neighborhood(search_result, neighborhood):
 
     return False
 
+def shuttle_stop_nearby(search_result, stop, max_distance):
+    """
+    Check if location is nearby a shuttle stop
+    """
+    if search_result['geotag']:
+        coordinates = search_result['geotag'] 
+    else:
+        return False
+
+    lat = coordinates[0]
+    lon = coordinales[1]
+     
+    stop_lat = shuttle_coords[stop][0]
+    stop_lon = shuttle_coords[stop][1]
+
+    distance = dist_coords(lat, lon, stop_lat, stop_lon)
+
+    if distance < max_distance:
+        return True
+
+def find_sf_neighborhood_housing():
+    """
+    San Francisco Housing BB
+    """
+    filtered_results = []
+    housing_results = get_housing_results('sf')
+    sf_hoods = neighborhood_coords['sf']
+
+    for hood in sf_hoods:
+        for result in housing_results:
+            if in_location_specified(result, sf_hoods[hood]):
+                filtered_results.append(result)
     
+    for hood in sf_hoods:
+        for result in housing_results:
+            if in_neighborhood(result, hood):
+                filtered_results.append(result)
 
+    return filtered_results
 
+def find_pen_neighborhood_housing():
+    """
+    San Francisco Housing BB
+    """
+    filtered_results = []
+    housing_results = get_housing_results('pen')
+    pen_hoods = neighborhood_coords['south_bay']
+
+    for hood in pen_hoods:
+        for result in housing_results:
+            if in_location_specified(result, pen_hoods[hood]):
+                filtered_results.append(result)
+    
+    for hood in pen_hoods:
+        for result in housing_results:
+            if in_neighborhood(result, hood):
+                filtered_results.append(result)
+
+    return filtered_results
---
+++ source/findHousing/utilities.py
@@ -0,0 +1,14 @@
+import math
+
+def dist_coords(lat1, lon1, lat2, lon2):
+    """
+    Find the distance between lattitudes and longitudes
+    """
+    lon1, lat1, lon2, lat2 = map(math.radians, [lon1, lat1, lon2, lat2])
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = math.sin(dlat/2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2) ** 2
+    c = 2 * math.asin(math.sqrt(a))
+    km = 6367 * c
+    return km
+
